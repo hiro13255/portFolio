d18ddd34b8763b75573b038ca1508de7eabb41ee
yuro-pc\yuitiro <yuichiro.n12@gmail.com>
2024-08-21 18:55:36 +0900

    skill カルーセル実装　（途中）

--- js/skill.js
+++ js/skill.js
@@ -1,18 +1,39 @@
-const swiper = new Swiper('.swiper', {
-    direction: 'horizontal',
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    scrollbar: {
-      el: '.swiper-scrollbar',
-      draggable: true,
-    },
-  });
-  
+const swiper = new Swiper(".swiper", {
+  // pagination: {
+  //     el: ".swiper-pagination"
+  //   },
+  //   effect: "coverflow",
+  //   grabCursor: true,
+  //   centeredSlides: true,
+  //   slidesPerView: "2",
 
+  //   coverflowEffect: {
+  //     rotate: 50,
+  //     stretch: 0,
+  //     depth: 100,
+  //     modifier: 1,
+  //     slideShadows: true
+  //   },
+
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev"
+  //   }
+  // });
+
+  slidesPerView: 1.9, //画像を何枚表示するか
+  spaceBetween: 20, //何ピクセル画像の間隔をあけるか
+  centeredSlides: true, //見切らせたい場合メイン画像をセンターにもってくるか
+  
+  //ページネーションをつける場合
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  //左右のナビゲーションをつける場合
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
